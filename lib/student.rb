9ae3662b48fde293b874f9eb6e9e3224a6cd21db
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-26 03:30:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
     def self.find_by_name(name)
         # binding.pry [[id, name, grade]]
         sql = "select * from students where name = ?"
-        DB[:conn].execute(sql, name).map { |row|  self.new_from_db(row)}
+        DB[:conn].execute(sql, name).map { |row|  self.new_from_db(row)}.first
     end
 
     def update
